e677f367e7a5ff058af48aaa016e70b80374a3e6
Aleksey Mishanin <mishanin_a_a@mail.ru>
2019-09-14 14:54:40 +0300

    Выполнено дз в рамках 6-го урока.

--- src/main/java/com/mishanin/springdata/repositories/ProductRepository.java
+++ src/main/java/com/mishanin/springdata/repositories/ProductRepository.java
@@ -2,22 +2,10 @@ package com.mishanin.springdata.repositories;
 
 import com.mishanin.springdata.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository("productRepository")
-public interface ProductRepository extends JpaRepository <Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
 
-    @Query("FROM Product p where p.cost = (select min(a.cost) from Product a)")
-    Product findProductByCostIsMin();
-
-    @Query("FROM Product p where p.cost = (select max (a.cost) from Product a)")
-    Product findProductByCostIsMax();
-
-    List<Product> findProductsByCostBetween(int min, int max);
-
-    @Override
-    long count();
 }
